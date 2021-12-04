8e8abbc6231691157973cfcfe7199dd892e1d675
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-04 10:51:34 +0700

    get sentiment

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,29 @@
 // @ts-ignore
-import aposToLexForm from "apos-to-lex-for"
-import { WordTokenizer } from "natural"
-
+import aposToLexForm from "apos-to-lex-form"
+import { WordTokenizer, SentimentAnalyzer, PorterStemmer } from "natural"
 // @ts-ignore
 import SpellCorrector from "spelling-corrector";
+import stopword from "stopword";
 
 const tokenizer = new WordTokenizer();
 const spellCorrector = new SpellCorrector();
 spellCorrector.loadDictionary();
 
-function getSentiment(str: String): -1 | 0 | 1 {
+const getSentiment = (str: String): -1 | 0 | 1 => {
     if (!str.trim()) {
         return 0;
     }
     const lexed = aposToLexForm(str).toLowerCase().replace(/[^a-zA-Z\s]+/g, "");
 
     const tokenized = tokenizer.tokenize(lexed);
+
+    const fixedSpelling = tokenized.map((word) => spellCorrector.correct(word));
+
+    const stopWordsRemoved = stopword.removeStopwords(fixedSpelling);
+
+    console.log(stopWordsRemoved);
+
+    return 0;
 }
+
+console.log(getSentiment("This is awesome!"));
